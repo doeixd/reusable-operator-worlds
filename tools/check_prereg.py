87f3bc13dd4c61cbdce8c72595c35be4d4d6866a
Patrick Glenn <doeixd@gmail.com>
2026-08-21 15:11:53 -0400

    prereg: freeze H39 existence plan

--- tools/check_prereg.py
+++ tools/check_prereg.py
@@ -31,6 +31,7 @@ FROZEN = {
     "V4R_CONFIRMATION_PLAN.md": "2aec65c",
     "V5_CONFIRMATION_PLAN.md": "1ed227d",
     "V6R_ADAPTATION_GEOMETRY_PLAN.md": "4c1bfaa",
+    "H39_EXISTENCE_PLAN.md": "b6fc27d",
 }
 
 STATUS_PATH_PATTERN = re.compile(
