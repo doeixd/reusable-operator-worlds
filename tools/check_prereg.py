6f3b0ae78e402be7a64e1b851d26a3ff90b55e7d
Patrick Glenn <doeixd@gmail.com>
2026-08-21 15:29:35 -0400

    prereg: freeze H39 world-0 pilot plan

--- tools/check_prereg.py
+++ tools/check_prereg.py
@@ -32,6 +32,7 @@ FROZEN = {
     "V5_CONFIRMATION_PLAN.md": "1ed227d",
     "V6R_ADAPTATION_GEOMETRY_PLAN.md": "4c1bfaa",
     "H39_EXISTENCE_PLAN.md": "16906ff",
+    "H39_PILOT_PLAN.md": "effaf6b",
 }
 
 STATUS_PATH_PATTERN = re.compile(
